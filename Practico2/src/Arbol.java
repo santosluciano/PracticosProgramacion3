04324af312402d84175df73dc27a335e7f83e5ad
santosluciano <luchosan74@gmail.com>
2018-04-17 16:34:42 -0300

    se comenzo a hacer el practico 2

---
+++ Practico2/src/Arbol.java
@@ -0,0 +1,21 @@
+
+public class Arbol {
+
+	//	public boolean hasElemClase (Object o) {
+	//	return raiz.hasElemClase(raiz, o);
+	//}
+	//
+	//private boolean hasElemClase (NodoArbol raiz, Object o) {
+	//	if (raiz != null) {
+	//		if (raiz.getInfo().equals(o)) {
+	//			return true;
+	//		}
+	//		else if (raiz.getInfo() < o) {
+	//			return hasElemClase (raiz.getIzq(), o);
+	//		}
+	//		else{
+	//			return hasElemClase (raiz.getDer(), o);
+	//		}
+	//	}
+	//}
+}
---
+++ Practico2/src/NodoArbol.java
@@ -0,0 +1,13 @@
+
+public class NodoArbol {
+	
+	private Object info;
+	private NodoArbol izq;
+	private NodoArbol der;
+	
+	public NodoArbol () {
+		this.info = null;
+		this.izq = null;
+		this.der = null;
+	}
+}
